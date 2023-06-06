c8af11b0d59c01008f3d8541b54dbb0be7c38005
Vinamra Mishra <vinamramishra2011@gmail.com>
2023-06-06 12:35:42 +0530

    bug fixed

--- components/Task.js
+++ components/Task.js
@@ -95,7 +95,7 @@ const styles = StyleSheet.create({
     del: {
         width: 20,
         height: 20,
-        marginLeft: 265,
+        marginLeft: 235,
         marginTop: 2.5
     },
     top: {
